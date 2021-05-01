73c35f55d637da7a7021de24d2a4ab87ba19c237
baron070981 <baron070981@gmail.com>
2021-05-02 00:29:28 +0300

    добавил класс Path

--- actionprocessing.py
+++ actionprocessing.py
@@ -1,6 +1,8 @@
 import pygame
 from random import randint
 from pprint import pprint
+import os
+import os.path
 
 
 '''
@@ -21,6 +23,16 @@ class Collisions:
         if pygame.Rect.colliderect(obj1.collide, obj2.collide):
             return True, obj1, obj2
         return False, None, None
+
+
+
+class Path:
+    
+    def __init__(self):
+        self.Home = os.path.abspath(os.path.dirname(__file__))
+    
+    def add(self, path_to_file):
+        return os.path.join(self.Home, path_to_file)
 
 
 
